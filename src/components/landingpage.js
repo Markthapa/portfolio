dba18261721a1f8465e04b427973e59b31e69952
Markthapa <markusthapa007@gmail.com>
2020-05-25 10:46:44 +0545

    Create a React app and publish it to GitHub Pages

---
+++ src/components/landingpage.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
+
+
+class Landing extends Component {
+  render() {
+    return(
+      <div style={{width: '100%', margin: 'auto'}}>
+        <Grid className="landing-grid">
+          <Cell col={12}>
+            <img
+              src="https://img.techpowerup.org/200525/oie-oie-trim-image.png"
+              alt="avatar"
+              className="avatar-img"
+              />
+
+            <div className="banner-text">
+              <h1>Hy, Welcome</h1>
+
+            <hr/>
+
+          <p>This is a simple page made using React, a javascript library.<br></br>
+        Join me on,</p>
+
+        <div className="social-links">
+
+          {/* LinkedIn */}
+          <a href="https://np.linkedin.com/in/markus-thapa-39a49b146" rel="noopener noreferrer" target="_blank">
+            <i className="fa fa-linkedin-square" aria-hidden="true" />
+          </a>
+
+          {/* Github */}
+          <a href="https://github.com/Markthapa" rel="noopener noreferrer" target="_blank">
+            <i className="fa fa-github-square" aria-hidden="true" />
+          </a>
+          {/*Facebook*/}
+          <a href="https://www.facebook.com/useles.maan/"rel="noopener noreferrer" target="_blank">
+            <i className="fa fa-facebook-square" aria-hidden="true" />
+                      </a>
+
+    
+          {/* Instagram*/}
+          <a href="https://www.instagram.com/sucram420/" rel="noopener noreferrer" target="_blank">
+          <i class="fa fa-instagram" aria-hidden="true"/>
+            </a>
+
+        </div>
+            </div>
+          </Cell>
+        </Grid>
+      </div>
+    )
+  }
+}
+
+export default Landing;
---
+++ src/components/resume.js
@@ -0,0 +1,97 @@
+import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
+import Education from './education';
+import Experience from './experience';
+import Skills from './skills';
+
+
+class Resume extends Component {
+  render() {
+    return(
+      <div>
+        <Grid>
+          <Cell col={4}>
+            <div style={{textAlign: 'center'}}>
+              <img
+                src="https://imagehost.com.au//azCmW"
+                alt="avatar"
+                style={{height: '400px'}}
+                 />
+            </div>
+
+            <h2 style={{paddingTop: '2em'}}>Mark Thapa</h2>
+            <h4 style={{color: 'grey'}}>Programmer</h4>
+            <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
+            <p>I Like good memes and good Music</p>
+            <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
+            <h5>Address</h5>
+            <p>Nepal,lalitpur</p>
+            <h5>Phone</h5>
+            <p>9818*****</p>
+            <h5>Email</h5>
+            <p>markusthapa007@gmail.com.com</p>
+            
+            <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
+          </Cell>
+          <Cell className="resume-right-col" col={8}>
+            <h2>Education</h2>
+
+
+            <Education
+              startYear={2002}
+              endYear={2006}
+              schoolName="My University"
+              schoolDescription=""
+               />
+
+               <Education
+                 startYear={2007}
+                 endYear={2009}
+                 schoolName="My 2nd University"
+                 schoolDescription=""
+                  />
+                <hr style={{borderTop: '3px solid #e22947'}} />
+
+              <h2>Experience</h2>
+
+            <Experience
+              startYear={2009}
+              endYear={2012}
+              jobName="First Job"
+              jobDescription=""
+              />
+
+              <Experience
+                startYear={2012}
+                endYear={2016}
+                jobName="Second Job"
+                jobDescription=""
+                />
+              <hr style={{borderTop: '3px solid #e22947'}} />
+              <h2>Skills</h2>
+              <Skills
+                skill="javascript"
+                progress={55}
+                />
+                <Skills
+                  skill="HTML/CSS"
+                  progress={60}
+                  />
+                  <Skills
+                   skill="React"
+                   progress={55}
+                    />
+                    <Skills
+                     skill="Procrastination"
+                     progress={99}
+                      />
+
+
+          </Cell>
+        </Grid>
+      </div>
+    )
+  }
+}
+
+export default Resume;
